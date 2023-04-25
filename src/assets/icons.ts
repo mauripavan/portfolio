5094901b32f7f29bbf456117816c033a5cae41ae
mauripavan <mauriciopavan60@gmail.com>
2023-04-25 15:11:10 -0300

    feat: adding icons & images

---
+++ src/assets/icons.ts
@@ -0,0 +1,13 @@
+const arrowDown = require("./icons/arrow-down.png");
+const burger = require("./icons/burger-bar.png");
+const close = require("./icons/close.png");
+const github = require("./icons/github.png");
+const linkedin = require("./icons/linkedin.png");
+
+export const icons = {
+  arrowDown,
+  burger,
+  close,
+  github,
+  linkedin,
+};
---
+++ src/assets/images.tsx
@@ -0,0 +1,5 @@
+const profile = require("./images/profile.png");
+
+export const images = {
+  profile,
+};
