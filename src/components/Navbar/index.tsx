433c8ba838031056dbb20e438129838484649551
mauripavan <mauriciopavan60@gmail.com>
2023-05-08 12:50:23 -0300

    style: small changes on navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { RxHamburgerMenu } from "react-icons/rx";
 import { FaCode } from "react-icons/fa";
 import { NavbarButtonType } from "./types";
 import { scrollToSection } from "@component/utils/useScroll";
@@ -18,6 +17,10 @@ export const navbarButtons: NavbarButtonType[] = [
     to: "portfolio",
   },
   {
+    name: "Contact",
+    to: "contact",
+  },
+  {
     name: "CV",
     to: "",
   },
@@ -25,25 +28,24 @@ export const navbarButtons: NavbarButtonType[] = [
 const NavBar = () => {
   return (
     <div className="flex h-28 items-center justify-center font-space font-normal text-xl">
-      <div className="flex w-full mx-28 justify-between tracking-wider">
-        <div className="flex flex-1 items-center gap-2">
+      <div className="flex flex-1 w-full mx-28 justify-between tracking-wider">
+        <div className="flex flex-1 items-center gap-2 z-30">
           <FaCode className="w-9 h-9 text-blue" />
           <p className="font-bold text-2xl"> Developer X</p>
         </div>
-        <div className="flex flex-1 items-center hover:text-gray-0 transition duration-150">
+        <div className="flex flex-1 justify-center items-center hover:text-gray-0 transition duration-150">
           {navbarButtons.map((button, i) => {
             return (
               <div
                 onClick={() => scrollToSection(button.to)}
-                className="flex flex-1 justify-center hover:text-white transition duration-150 cursor-pointer"
+                className={`flex flex-1 justify-center hover:text-white transition-all duration-10 cursor-pointer`}
               >
-                <p key={i}>{button.name}</p>
+                <p className="text-center" key={i}>
+                  {button.name}
+                </p>
               </div>
             );
           })}
-          <div>
-            <RxHamburgerMenu className="w-6 h-6 text-white" />
-          </div>
         </div>
       </div>
     </div>
